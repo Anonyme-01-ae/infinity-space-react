841394cfbc0fba6d44cc17c2ccc814c851e608c0
Anonyme-01-ae <benhaddou.anass0@gmail.com>
2026-06-03 23:55:51 +0100

    Update App component to include data props
    Pass members and events data as props to Members and Events components.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -7,14 +8,17 @@ import Events from "./components/Events";
 import JoinForm from "./components/JoinForm";
 import Footer from "./components/Footer";
 
+import { members, pastEvents, futureEvents } from "./data";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
-      <Members />
-      <Events />
+      <Members members={members} />
+      <Events title="Activités réalisées" events={pastEvents} bgLight={true} />
+      <Events title="Activités à venir" events={futureEvents} bgLight={false} />
       <JoinForm />
       <Footer />
     </>
