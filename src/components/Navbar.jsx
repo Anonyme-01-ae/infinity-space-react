2d5f717fd5cbb4068d03224ff4b15ec56707015a
Anonyme-01-ae <benhaddou.anass0@gmail.com>
2026-06-04 06:58:24 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,49 @@
 function Navbar() {
-  return <h2>Navbar</h2>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <div className="container">
+        <a className="navbar-brand fw-bold d-flex align-items-center gap-2" href="#accueil">
+          <img src="/images/logo.png" alt="Logo Infinity Space" className="logo-img" />
+          Infinity Space
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+          aria-controls="mainNav"
+          aria-expanded="false"
+          aria-label="Menu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="mainNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#accueil">Accueil</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#apropos">À propos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#bureau">Bureau</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#activites">Activités</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#avenir">À venir</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
